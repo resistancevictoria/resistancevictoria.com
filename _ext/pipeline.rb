65951c0e4930254e63602553995312102077c65f
Scott Weston <scottw@gmail.com>
2013-04-16 22:37:53 +1000

    google analytics and g+ link

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -2,5 +2,8 @@
 Awestruct::Extensions::Pipeline.new do
   # extension Awestruct::Extensions::Posts.new( '/news' ) 
   # extension Awestruct::Extensions::Indexifier.new
+
+  # helpers
+  helper Awestruct::Extensions::GoogleAnalytics
 end
 
