85b86c48e22a3df9a5f6426c50d2eb3b61cb0e6f
Scott Weston <scottw@gmail.com>
2013-12-03 14:16:22 +1100

    parallax effect

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+  // cache the window object
+  $window = $(window);
+
+  $('section[data-type="background"]').each(function(){
+    // declare the variable to affect the defined data-type
+    var $scroll = $(this);
+
+    $(window).scroll(function() {
+      // HTML5 proves useful for helping with creating JS functions!
+      // also, negative value because we're scrolling upwards
+      var yPos = -($window.scrollTop() / $scroll.data('speed'));
+
+      // background position
+      var coords = '50% '+ yPos + 'px';
+
+      // move the background
+      $scroll.css({ backgroundPosition: coords });
+    }); // end window scroll
+  });  // end section function
+
   $("a").click(function() {
     link_host = this.href.split("/")[2];
     document_host = document.location.href.split("/")[2];
@@ -9,3 +29,4 @@ $(document).ready(function() {
     }
   });
 });
+
